2a737375108de5ace6db67b9e40cf82e665f6ffc
coco483 <151052125+coco483@users.noreply.github.com>
2024-07-01 16:08:29 +0900

    backup

--- app/src/main/java/com/example/viewactivitypractice/fragments/PostAddPage.kt
+++ app/src/main/java/com/example/viewactivitypractice/fragments/PostAddPage.kt
@@ -3,6 +3,7 @@ package com.example.viewactivitypractice.fragments
 import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
@@ -18,6 +19,8 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.viewactivitypractice.DataBaseHandler
 import com.example.viewactivitypractice.MainActivity
 import com.example.viewactivitypractice.R
@@ -32,7 +35,7 @@ import com.example.viewactivitypractice.uriToBitmap
 class PostAddPage : Fragment() {
     private lateinit var myDB: DataBaseHandler
     private val pickupimage = 100
-    private var ImageURI : Uri? = null
+    private val takePicture = 101
     private var bitImg : Bitmap? = null
     private var imgId: Int? = null
     private lateinit var imgView : ImageView
@@ -48,10 +51,32 @@ class PostAddPage : Fragment() {
         val uploadBtn = view.findViewById<Button>(R.id.post_upload_btn)
         val cancelBtn = view.findViewById<Button>(R.id.post_cancel_btn)
         imgView = view.findViewById<ImageView>(R.id.post_imageView)
+        /*
         imgView.setOnClickListener{
             val pickupImgFromGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             startActivityForResult(pickupImgFromGallery, pickupimage)
         }
+         */
+        imgView.setOnClickListener {
+            val cameraPermissionCheck = ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CAMERA
+            )
+
+            if (cameraPermissionCheck != PackageManager.PERMISSION_GRANTED) { // 권한이 없는 경우
+                Log.d("postCam", "request for camera permission")
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    1000
+                )
+            } else { // 권한이 있는 경우
+                Log.d("postCam", "we have camera permission")
+                val takePictueByCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(takePictueByCamera, takePicture)
+            }
+        }
+
         uploadBtn.setOnClickListener{
             val content = view.findViewById<EditText>(R.id.content_ET).text.toString()
             if (content.length == 0) {
@@ -71,14 +96,38 @@ class PostAddPage : Fragment() {
     // 사진 가져오면 imgData에 추가하기
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == pickupimage){
-            ImageURI = data?.data
-            Log.d("IMG_PICK", "successfully picked an img")
-            //imgDataList.add(ImageData(484, uriToBitmap(ImageURI)) )
-            bitImg = uriToBitmap(ImageURI, requireActivity())
-            if (bitImg!=null) imgView.setImageBitmap(bitImg)
-            //imgId = myDB.insertImg(bitImg).toInt()
-            //Log.d("PostAddImg", "id $imgId added")
+        when(requestCode){
+            pickupimage -> {
+                if (resultCode == RESULT_OK) {
+                    val ImageURI: Uri? = data?.data
+                    Log.d("IMG_PICK", "successfully picked an img, $ImageURI")
+                    bitImg = uriToBitmap(ImageURI, requireActivity())
+                    if (bitImg!=null) imgView.setImageBitmap(bitImg)
+                }
+            }
+            takePicture -> {
+                if (resultCode == RESULT_OK){
+                    bitImg = data?.extras?.get("data") as Bitmap
+                    Log.d("CAMERA", "successfully took a picture, $bitImg")
+                    if (bitImg!=null) imgView.setImageBitmap(bitImg)
+                }
+            }
+        }
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1000) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val takePictueByCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(takePictueByCamera, takePicture)
+            } else { // 거부
+                Toast.makeText(requireContext(), "카메라 권한 거부", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
